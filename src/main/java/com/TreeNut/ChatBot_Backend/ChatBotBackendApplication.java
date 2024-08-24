4a74eaabd67126c1eb9bf5c59fb610cb25814c9a
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-08-24 15:40:52 +0900

    Publish

---
+++ src/main/java/com/TreeNut/ChatBot_Backend/ChatBotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TreeNut.ChatBot_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotBackendApplication.class, args);
+	}
+
+}
