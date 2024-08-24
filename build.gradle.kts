c645a880ed93f0ae3b7d00a0a832876bd194b9aa
mhj228 <mhj2281@naver.com>
2024-08-24 16:04:10 +0900

    Mysql 의존성

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,13 @@ dependencies {
 	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    
+    // MySQL 의존성 추가
+    implementation("mysql:mysql-connector-java:8.0.27") // 최신 버전에 맞게 변경 가능
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
