9f1b159148987fef2b5640c47770fdd3122df631
jgkim14 <48284333+jgkim14@users.noreply.github.com>
2024-08-24 17:24:48 +0900

    Update ChatBotBackendApplication.java

--- src/main/java/com/TreeNut/ChatBot_Backend/ChatBotBackendApplication.java
+++ src/main/java/com/TreeNut/ChatBot_Backend/ChatBotBackendApplication.java
@@ -2,8 +2,9 @@ package com.TreeNut.ChatBot_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ChatBotBackendApplication {
 
 	public static void main(String[] args) {
